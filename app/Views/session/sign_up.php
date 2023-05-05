0f4e2bb137d0446f315bc908e704e18e6d9a36b3
Eduardo Ocelotl <eocelotlvalencia@outlook.com>
2023-05-05 10:27:57 -0600

    Agregar fotter y logo

--- app/Views/session/sign_up.php
+++ app/Views/session/sign_up.php
@@ -9,6 +9,7 @@
 
   <!-- CDN via jsDelivr Bootstrap 5 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+ 
 </head>
 
 <body>
@@ -113,6 +114,33 @@
     </section>
   </main>
 
+<div class="d-flex flex-column flex-md-row justify-content-between py-0 px-0 px-xl-4 bg-dark text-center text-md-start" style="margin-top: 50px;">
+  <div class="order-md-1 mb-3 mb-md-0">
+    <img src="<?php echo base_url('assets/images/EREIICDI_LOGO.svg'); ?>" alt="EREIICDI" width="300" height="120">
+  </div>
+  <div class="order-md-2 mb-3 mb-md-0 text-white" style="text-align: center; display: flex; justify-content: center; margin-top: 50px;">
+  <p class="mx-4"> Copyright&copy;2023. EREIICDI</p>
+</div>
+<div class="order-md-3 mb-3 mb-md-0 text-white" style="margin-top: 50px;">
+  <ul class="list-unstyled d-flex justify-content-center justify-content-md-start">
+    <li class="mx-3">
+    <a href="https://www.facebook.com/mi-pagina" target="_blank"><i class="fab fa-facebook">Facebook</i></a>
+    </li>
+    <li class="mx-3">
+    <a href="https://www.twitter.com/mi-pagina" target="_blank"><i class="fab fa-twitter">Twitter</i></a>
+    </li>
+    <li class="mx-3">
+    <a href="https://www.instagram.com/mi-pagina" target="_blank"><i class="fab fa-instagram">Instagram</i></a>
+    </li>
+    <li class="mx-3">
+      <a href="mailto:contacto@ejemplo.com" target="_blank"><i class="far fa-envelope fa-2x">Correo</i></a>
+      </a>
+    </li>
+  </ul>
+</div>
+</div>
+
+  
   <!-- CDN via jsDelivr Bootstrap 5 -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
